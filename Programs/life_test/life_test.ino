9ed1e5f61721ee17aed52278a65e166d1387cef5
norn93 <george.knowlden@gmail.com>
2017-04-01 12:54:15 +0800

    Added a testing program

---
+++ Programs/life_test/life_test.ino
@@ -0,0 +1,14 @@
+#include "NanoBot.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  NanoInitialise();
+  _NanoInitialiseMotors();
+  NanoForward(500);
+  delay(500);
+  NanoStop();
+}
+
+void loop() {
+
+}
